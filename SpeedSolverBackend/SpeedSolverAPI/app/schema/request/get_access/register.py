ddc8d14926b0c65f5b8696898bb96be841c7fa73
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2024-12-22 13:04:37 +0700

    added email validation

--- SpeedSolverBackend/SpeedSolverAPI/app/schema/request/get_access/register.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/schema/request/get_access/register.py
@@ -1,6 +1,13 @@
-from pydantic import BaseModel
+import re
+from pydantic import BaseModel, field_validator
+from app.exc import bad_email
 
 class RegisterRequest(BaseModel):
     email: str
     password: str
 
+    @field_validator("email")
+    def validate_email(cls, attr):
+        if not re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", attr):
+            raise bad_email.BadEmail()
+        return attr
