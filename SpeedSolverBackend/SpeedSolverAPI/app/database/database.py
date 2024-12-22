3badf866d6a73299cf141e7cd74bb47b03ea5230
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2024-12-22 12:24:24 +0700

    edited models

--- SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
 from ..cfg.settings import settings
 
-def get_engine() -> AsyncEngine:
+async def get_engine() -> AsyncEngine:
     return create_async_engine(str(settings.db_url))
 
 async def get_session() -> Session:
-    return Session(bind=get_engine())
+    return Session(bind=await get_engine())
