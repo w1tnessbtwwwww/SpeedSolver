0018099651dadd23b65ab1ee5dff575cfc6075ed
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2024-12-22 13:24:47 +0700

    edited get_session(), returning AsyncSession (needs fix)

--- SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
@@ -1,10 +1,18 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    create_async_engine, 
+    AsyncEngine
+)
+
+from sqlalchemy.orm import sessionmaker
+
 
 from ..cfg.settings import settings
 
 async def get_engine() -> AsyncEngine:
     return create_async_engine(str(settings.db_url))
 
-async def get_session() -> AsyncSession:
-    return AsyncSession(bind=await get_engine())
+async def get_session():
+    async_session = sessionmaker(await get_engine(), expire_on_commit=False, class_=AsyncSession)
+    async with async_session() as session:
+        return session
