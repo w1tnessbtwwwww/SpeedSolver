1f309f3091a10bbbb9e25c40fac533f7ffbc00b6
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2024-12-22 13:24:53 +0700

    added await to executes

--- SpeedSolverBackend/SpeedSolverAPI/app/database/abstract/abc_repo.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/abstract/abc_repo.py
@@ -12,30 +12,30 @@ class AbstractRepository(ABC):
 
     async def commit(self):
         try:
-             self._session.commit()
+            await self._session.commit()
         except SQLAlchemyError as e:
-            self._session.rollback()
+            await self._session.rollback()
             raise e
 
     def rollback(self):
         self._session.rollback()
 
     async def get_by_id(self, id):
-        return self._session.get(self.model, id)
+        return await self._session.get(self.model, id)
 
     async def get_all(self):
-        result = self._session.execute(select(self.model))
+        result = await self._session.execute(select(self.model))
         return result.scalars().all()
 
     async def create(self, **kwargs):
         query = insert(self.model).values(**kwargs).returning(self.model)
-        result = self._session.execute(query)
+        result = await self._session.execute(query)
         await self.commit()
         return result.scalars().first()
 
     async def update_one(self, id, **kwargs):
         query = update(self.model).where(self.model.id == id).values(**kwargs).returning(self.model)
-        result = self._session.execute(query)
+        result = await self._session.execute(query)
         return result.scalars().first()
 
     async def delete_by_id(self, id):
@@ -44,7 +44,7 @@ class AbstractRepository(ABC):
 
     async def get_by_filter_all(self, **kwargs):
         query = select(self.model).filter_by(**kwargs)
-        result = self._session.execute(query)
+        result = await self._session.execute(query)
         return result.scalars().all()
 
     async def get_by_filter_one(self, **kwargs):
