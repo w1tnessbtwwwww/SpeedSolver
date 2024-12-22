0fadbe50dde11fdc9aebda2064466e8ae72cbb22
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-12-22 18:39:22 +0700

    hotfixed imports, added jwt import

--- SpeedSolverBackend/SpeedSolverAPI/app/routing/access_router.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/routing/access_router.py
@@ -9,14 +9,16 @@ from app.database.database import get_session
 from app.schema.response.AccessToken import AccessToken
 from app.schema.request.get_access.register import RegisterRequest
 
-
 from app.services.user_service import UserService
 
+from app.services.verification_service import VerificationService
 
 from app.utils.result import Result
+from app.utils.verify_codes_generator.code_generator import generate_confirmation_code 
 
 from app.routing.security.jwttype import JWTType
 from app.routing.security.jwtmanager import JWTManager, oauth2_scheme
+from app.routing.security.jwtmanager import oauth2_scheme
 
 from app.exc.bad_email import BadEmail
 
@@ -25,14 +27,26 @@ from sqlalchemy.ext.asyncio import AsyncSession
 auth_router = APIRouter(prefix="/access", tags=["System Access"])
 
 
+@auth_router.get("/email/confirm")
+async def confirm_email(code: str, email: str, session: AsyncSession = Depends(get_session)):
+    result = await VerificationService(session).confirm_email(email, code)
+    if not result.success:
+        raise HTTPException(status_code=400, detail=result.error)
+    
+    return {
+        "message": "Почта успешно подтверждена"
+    }
+
 @auth_router.post("/register")
 async def register(registerRequest: RegisterRequest, session: AsyncSession = Depends(get_session)):
     registered: Result = await UserService(session).register(registerRequest)
     if not registered.success:
         raise HTTPException(status_code=400, detail=registered.error)
     
+    verification: Result = await VerificationService(session).request_verification(registered.value.email, registered.value.userId)
+
     return {
-        "register": "User registered successfully"        
+        "register": "Вы успешно зарегистрировались. На указанную почту отправлен код подтвержедения."        
     }
 
 
@@ -68,12 +82,13 @@ async def refresh_access_token(request: Request):
         raise HTTPException(status_code=400, detail=token_data.error)
 
 
-    with await get_session() as session:
-        user = await UserRepository(session).get_by_filter_one(userId=token_data.value["userId"])
-        if not user:
-            raise HTTPException(status_code=400, detail="User not found")
-        
-        return jwt_manager.encode_token({ "userId": str(user.userId), "email": user.email }, token_type=JWTType.ACCESS)
+    session: AsyncSession = await get_session()
+    user = await UserRepository(session).get_by_filter_one(userId=token_data.value["userId"])
+    session.close()
+    if not user:
+        raise HTTPException(status_code=400, detail="User not found")
+    
+    return jwt_manager.encode_token({ "userId": str(user.userId), "email": user.email }, token_type=JWTType.ACCESS)
 
 @auth_router.get("/refresh")
 async def refresh(token: str = Depends(refresh_access_token)):
