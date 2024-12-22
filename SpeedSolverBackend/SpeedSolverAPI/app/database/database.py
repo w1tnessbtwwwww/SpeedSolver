6b35a77fadb5e62e9d14449a9409db229c947e02
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2024-12-22 13:03:30 +0700

    remaking to async connection

--- SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
 from ..cfg.settings import settings
@@ -6,5 +6,5 @@ from ..cfg.settings import settings
 async def get_engine() -> AsyncEngine:
     return create_async_engine(str(settings.db_url))
 
-async def get_session() -> Session:
-    return Session(bind=await get_engine())
+async def get_session() -> AsyncSession:
+    return AsyncSession(bind=await get_engine())
