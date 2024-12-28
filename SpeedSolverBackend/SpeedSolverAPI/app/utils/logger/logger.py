f719c0989cbb4030d2eb040abf5dca5d17a9e035
xxxw1tnessbtwxxx <89967499+xxxw1tnessbtwxxx@users.noreply.github.com>
2024-12-28 15:05:53 +0700

    revorking logger. setting to telegram

--- SpeedSolverBackend/SpeedSolverAPI/app/utils/logger/logger.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/utils/logger/logger.py
@@ -1,7 +1,5 @@
-import logging
-from logging import Logger
-import datetime
-logging.basicConfig(level=logging.INFO, filename=f"logs/{datetime.date.today()}.log",filemode="a",
-                    format="%(asctime)s %(levelname)s %(message)s")
 
-logger = logging.getLogger(__name__)
+
+class Logger:
+    def __init__(self):
+        ...
