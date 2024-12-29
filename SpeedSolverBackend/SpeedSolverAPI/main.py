ea6f7d229601cac6b60dc27619308c2566873b87
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2024-12-29 13:49:34 +0700

    removed test

--- SpeedSolverBackend/SpeedSolverAPI/main.py
+++ SpeedSolverBackend/SpeedSolverAPI/main.py
@@ -1,22 +1,27 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 
 from app.cfg.settings import settings
+from app.database.database import get_session
 from app.exc.bad_email import BadEmail
 from app.routing.main_router import main_router
 from app.utils.email_service.email_service import EmailService
 
 from starlette.middleware.cors import CORSMiddleware
 
+from typing import Annotated
+from sqlalchemy.orm import Session
 from alembic.config import Config
 from alembic import command
+
+
 
 api = FastAPI(
     title="SpeedSolverAPI",
     description="The API docs for SpeedSolver.",
     version="v1",
 )
-
 
 
 api.add_middleware (
@@ -29,6 +34,7 @@ api.add_middleware (
     allow_methods=["*"],
     allow_headers=["*"]
 )
+
 
 api.include_router(main_router)
 
