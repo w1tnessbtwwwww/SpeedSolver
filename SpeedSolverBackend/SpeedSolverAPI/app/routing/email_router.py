11124745edbfbd3314de0f7bb4e635bf4bb49ded
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-02 19:11:26 +0700

    dividing email and confirmation routes

--- SpeedSolverBackend/SpeedSolverAPI/app/routing/email_router.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/routing/email_router.py
@@ -1,16 +1,15 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
+from app.database.database import get_session
 from app.routing.security.jwtmanager import JWTManager, oauth2_scheme
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 email_router = APIRouter(
     prefix="/email",
     tags=["Email"]
 )
 
-@email_router.post("/confirm")
-async def confirm_email(user_id: str, code: str, token: str = Depends(oauth2_scheme)):
-    user = JWTManager().get_current_user()
-
 @email_router.post("/change")
-async def change_email(user_id: str, email: str):
-    ...
+async def change_email(token: str = Depends(oauth2_scheme), session: AsyncSession = Depends(get_session)):
+    raise HTTPException(status_code=400, detail="Not implemented")
