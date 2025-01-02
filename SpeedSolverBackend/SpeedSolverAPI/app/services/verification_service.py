c4cf75fda86214d7a025e310762d9f4056f25462
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-02 19:12:52 +0700

    renamed create_verification to process, changed logic

--- SpeedSolverBackend/SpeedSolverAPI/app/services/verification_service.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/services/verification_service.py
@@ -4,7 +4,7 @@ from app.database.repo.verification_repository import VerificationRepository
 from app.utils.result import Result, err, success
 from app.utils.email_service.email_service import EmailService
 from app.utils.logger.telegram_bot.telegram_logger import logger
-
+from app.utils.verify_codes_generator.code_generator import generate_confirmation_code
 from app.routing.security.jwtmanager import JWTManager
 class VerificationService:
     def __init__(self, session: AsyncSession):
@@ -12,9 +12,14 @@ class VerificationService:
         self._repo: VerificationRepository = VerificationRepository(session)
 
 
-    async def create_verification(self, user_id: str, email: str):
-        verification = await EmailService().send_verify_code("Подтверждение регистрации", email)
-        if not verification.success:
-            logger.error(verification.error)
-            return err(verification.error)
-        return await self._repo.insert_verification(user_id, verification.value)
+    async def process_verification(self, userId: str, email: str):
+        code = await EmailService().send_verify_code("Подтверждение письма", email)
+        if not code.success:
+            logger.fatal(code.error)
+            return
+
+        verification = await self._repo.process_verification(userId, code.value)
+        return verification
+    
+    async def confirm_email(self, userId: str, code: str) -> Result[None]:
+        return await self._repo.confirm_email(userId, code)
