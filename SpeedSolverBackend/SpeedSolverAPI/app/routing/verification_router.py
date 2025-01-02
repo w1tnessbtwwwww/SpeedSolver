6e039ddae3ffc3890f49d60c8e6d9367d715379b
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-02 23:01:45 +0700

    added resend method

--- SpeedSolverBackend/SpeedSolverAPI/app/routing/verification_router.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/routing/verification_router.py
@@ -3,10 +3,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.database import get_session
+
+from app.database.models.models import User
 from app.schema.request.email.confirm_email import EmailConfirmation
+
 from app.services.user_service import UserService
 from app.services.verification_service import VerificationService
 
+from app.routing.security.jwtmanager import JWTManager, get_current_user, oauth2_scheme
 
 verification_router = APIRouter(
     prefix="/verification",
@@ -14,8 +18,14 @@ verification_router = APIRouter(
 )
 
 @verification_router.post("/resend")
-async def resend_verification():
-    raise HTTPException(status_code=400, detail="Not implemented")
+async def resend_verification(user: User = Depends(JWTManager().get_current_user), session: AsyncSession = Depends(get_session)):
+    
+    result = await VerificationService(session).resend_verification(user.value.userId, user.value.email)
+    
+    return {
+        "message": "Письмо успешно отправлено."
+    }
+
 
 @verification_router.post("/confirm")
 async def confirm_verification(confirmRequest: EmailConfirmation, session: AsyncSession = Depends(get_session)):
