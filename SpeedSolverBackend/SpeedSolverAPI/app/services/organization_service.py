1b786a485ad881668b4cc7ea68786f2dc421a1aa
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-05 16:18:13 +0700

    new imports, creating and updating

--- SpeedSolverBackend/SpeedSolverAPI/app/services/organization_service.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/services/organization_service.py
@@ -7,6 +7,8 @@ from app.database.repo.organization_repository import OrganizationRepository
 from app.routing.security.jwtmanager import JWTManager
 
 from app.schema.request.organization.create_organization import CreateOrganization
+from app.schema.request.organization.update_organization import UpdateOrganization
+from app.utils.result import Result, err, success
 
 
 class OrganizationService:
@@ -15,6 +17,18 @@ class OrganizationService:
         self._session = session
         self._repo = OrganizationRepository(session)
 
-    async def create_organization(self, createRequest: CreateOrganization, token: str):
-        user: User = await JWTManager().get_current_user(token, self._session)
-        return await self._repo.create_organization(title=createRequest.name, description=createRequest.description, leaderId=user.userId)
+    async def create_organization(self, createRequest: CreateOrganization, userId: str) -> Result[Organization]:
+        return await self._repo.create_organization(title=createRequest.name, description=createRequest.description, leaderId=userId)
+    
+    async def update_organization(self, updateRequest: UpdateOrganization, userId: str) -> Result[Organization]:
+        organization = await self._repo.get_by_filter_one(organizationId=updateRequest.organizationId, leaderId=userId)
+
+        if not organization:
+            return err("Организация не найдена.")
+    
+        organization_upd = await self._repo.update_organization(organizationId=updateRequest.organizationId, title=updateRequest.title, description=updateRequest.description, leaderId=userId)
+        if not organization_upd.success:
+            return err(organization_upd.error)
+        
+        return success(organization_upd.value)
+        
