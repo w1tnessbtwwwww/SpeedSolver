55c4328938c4c3210bd85e41329b020521c89185
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-07 20:36:59 +0700

    revorked creating and updating

--- SpeedSolverBackend/SpeedSolverAPI/app/routing/team_router.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/routing/team_router.py
@@ -17,16 +17,25 @@ team_router = APIRouter(prefix="/team", tags=["Teams management"])
 
 @team_router.post("/create")
 async def create_team(createRequest: CreateTeam, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
-    creating = await TeamService(session).create_team(createRequest)
+    
+    creating = await TeamService(session).create_team(createRequest, user.userId)
     if not creating.success:
         raise HTTPException(status_code=400, detail=creating.error)
     
     return creating.value
 
 @team_router.put("/update")
-async def update_team(updateRequest: UpdateTeam, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
-    updating = await TeamService(session).update_team(updateRequest)
+async def update_team(team_id: str,updateRequest: UpdateTeam, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
+    updating = await TeamService(session).update_team(updateRequest, user.userId, team_id)
     if not updating.success:
         raise HTTPException(status_code=400, detail=updating.error)
     
     return updating.value
+
+@team_router.delete("/delete")
+async def delete_team(team_id: str, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
+    deleting = await TeamService(session).delete_team(team_id, user.userId)
+    if not deleting.success:
+        raise HTTPException(status_code=400, detail=deleting.error)
+    
+    return deleting.value
