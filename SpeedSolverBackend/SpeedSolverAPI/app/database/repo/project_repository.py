19f0ee8e918e0a1b1024a1d35544f3095471a1bb
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-09 13:26:21 +0700

    work in progress: project creation

--- SpeedSolverBackend/SpeedSolverAPI/app/database/repo/project_repository.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/repo/project_repository.py
@@ -3,40 +3,42 @@ from app.database.abstract.abc_repo import AbstractRepository
 from app.database.models.models import Project
 from app.database.repo.team_projects_repository import TeamProjectRepository
 from app.utils.result import Result, err, success
+from app.utils.logger.telegram_bot.telegram_logger import logger
 
 
 class ProjectRepository(AbstractRepository):
     model = Project
 
 
-    async def create_project(self, title: str, description: str) -> Result[Project]:
+    async def create_project(self, binded_teamId: str, title: str, description: str) -> Result[Project]:
+
+        team_project_repository = TeamProjectRepository(self._session)
+
+
         project_query = (
             select(self.model)
-            .where(and_(
-                self.model.title == title,
-                self.model.decsription == description
-            ))
+            .where(
+                and_(
+                    self.model.title == title,
+                    self.model.description == description
+                )
+            )
         )
 
         project_result = await self._session.execute(project_query)
-        project = project_result.scalars().first()
+        projects = project_result.scalars().all()
 
-        if not project:
-            return success(value = await self.create(title=title, description=description))
-        
-        team_project_repo = TeamProjectRepository(self._session)
 
-        project_assign_query = (
-            select(team_project_repo.model)
-            .where(and_(
-                team_project_repo.model.projectId == project.projectId
-            ))
+        team_projects_query = (
+            select(team_project_repository.model)
+            .where(
+                team_project_repository.model.teamId == binded_teamId
+            )
         )
 
-        project_assign_result = await self._session.execute(project_assign_query)
-        project_assign = project_assign_result.scalars().first()
+        team_projects_result = await self._session.execute(team_projects_query)
+        team_projects = team_projects_result.scalars().all()
 
-        if not project_assign:
-            return success(value = await self.create(title=title, description=description))
+
         
-        return err("Проект с таким названием у команды уже существует")
+        
