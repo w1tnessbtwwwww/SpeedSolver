bb42e1805419106b255592c33f3972563f27a472
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-09 13:28:00 +0700

    in progress: creating project

--- SpeedSolverBackend/SpeedSolverAPI/app/routing/project_router.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/routing/project_router.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.database.database import get_session
 from app.database.models.models import User
-from app.routing.security.jwtmanager import get_current_user
+from app.security.jwtmanager import get_current_user
 from app.schema.request.project.create_project import CreateProject
 
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.services.project_service import ProjectService
 
 project_router = APIRouter(
     prefix="/projects",
@@ -13,4 +15,4 @@ project_router = APIRouter(
 
 @project_router.post("/create")
 async def create_project(create_project: CreateProject, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
-    ...
+    creating = await ProjectService(session).create_project(create_project.for_team, create_project.title, create_project.description)
