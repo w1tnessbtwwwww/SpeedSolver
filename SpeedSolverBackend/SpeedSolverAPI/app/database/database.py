9b17476542e3bb42a6dd8151f276f05b9edf188f
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-11 23:30:14 +0700

    added automigration

--- SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/database.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     create_async_engine, 
@@ -8,9 +9,13 @@ from sqlalchemy.orm import sessionmaker
 
 
 from app.cfg.settings import settings
+from app.utils.logger.telegram_bot.telegram_logger import logger
+from app.database.models.models import Base
 
 async def get_engine() -> AsyncEngine:
     return create_async_engine(str(settings.db_url))
+
+
 
 async def get_session():
     async_session = sessionmaker(await get_engine(), class_=AsyncSession, expire_on_commit=False)
@@ -19,3 +24,11 @@ async def get_session():
             yield session
         finally:
             await session.close()
+
+async def create_tables():
+    try:
+        engine = await get_engine()
+        async with engine.begin() as eng:
+            await eng.run_sync(Base.metadata.create_all)
+    except Exception as e:
+        logger.fatal("Не удалось создать таблицы. ", str(e))
