bc37684a815e03cf717b5bd49ccf8723d808ceeb
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-12 13:28:13 +0700

    get_team_by_project func

--- SpeedSolverBackend/SpeedSolverAPI/app/database/repo/team_projects_repository.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/database/repo/team_projects_repository.py
@@ -1,7 +1,19 @@
+from sqlalchemy import Result, select
 from app.database.abstract.abc_repo import AbstractRepository
-from app.database.models.models import TeamProject
+from app.database.models.models import Team, TeamProject
+from app.utils.result import err, success
 
 class TeamProjectRepository(AbstractRepository):
     model = TeamProject
 
-    
+    async def get_team_by_project(self, projectId: str) -> Result[Team]:
+        try:
+            query = (
+                select(Team)
+                .join(TeamProject, TeamProject.projectId == projectId)
+            )
+            team = await self._session.execute(query)
+            teamProject = team.scalars().all()
+            return success(teamProject)
+        except Exception as e:
+            return err(str(e))
