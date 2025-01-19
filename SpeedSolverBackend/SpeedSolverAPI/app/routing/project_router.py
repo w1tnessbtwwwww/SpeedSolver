12852f18c0f6b6d21d9b77d9acde6afb16aa1e32
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-01-19 19:38:31 +0700

    added project_router

--- SpeedSolverBackend/SpeedSolverAPI/app/routing/project_router.py
+++ SpeedSolverBackend/SpeedSolverAPI/app/routing/project_router.py
@@ -1,16 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.routing.objective_router import objective_router
+
 from app.database.database import get_session
 from app.database.models.models import User
+
 from app.schema.request.objective.create_objective import CreateObjective
 from app.schema.request.project.update_project import UpdateProject
-from app.security.jwtmanager import get_current_user
 from app.schema.request.project.create_project import CreateProject
-
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.services.objective_service import ObjectiveService
 from app.services.project_service import ProjectService
+
+from app.security.jwtmanager import get_current_user
+
+
 
 project_router = APIRouter(
     prefix="/projects",
@@ -33,19 +39,3 @@ async def update_project(project_id: str, update_project: UpdateProject, user: U
     
     return updating.value
 
-@project_router.get("/objectives/all")
-async def get_all_tasks(project_id: str, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
-    raise HTTPException(status_code=400, detail="Not implemented")
-
-@project_router.post("/objectives/create")
-async def create_task(project_id: str, create_request: CreateObjective, user: User = Depends(get_current_user), session: AsyncSession = Depends(get_session)):
-    creating = await ObjectiveService(session).create_objective(
-        project_id=project_id,
-        author_id=user.userId,
-        create_objective=create_request
-    )
-
-    if not creating.success:
-        raise HTTPException(status_code=400, detail=creating.error)
-    
-    return creating.value
