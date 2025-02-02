ad24ed72dcae97c3273ce0a33f8d1d20a67262ce
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-02-02 19:02:46 +0700

    changed button props, allow customs

--- SpeedSolverMVP/SpeedSolverFrontend/src/components/primaryButton/PrimaryButton.tsx
+++ SpeedSolverMVP/SpeedSolverFrontend/src/components/primaryButton/PrimaryButton.tsx
@@ -1,10 +1,16 @@
 import React from "react"
 import "./PrimaryButton.css"
 
-export const PrimaryButton: React.FC<{ text: string, onClick?: () => void }> = ({ text, onClick }) => {
+interface PrimaryButtonProps {
+    text: string;
+    onClick?: () => void;
+    className?: string;
+  }
+
+export const PrimaryButton: React.FC<PrimaryButtonProps> = ({ text, onClick, className }) => {
     return (
-        <button className="primaryButton" onClick={onClick}>
-            {text}
-        </button>
-    )    
-}
+      <button className={`primaryButton ${className || ''}`} onClick={onClick}>
+        {text}
+      </button>
+    );
+  };
