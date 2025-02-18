4c96e82c68dc2d20563c8d19ef056e9f43dab603
w1tnessbtwwwww <work.w1tnessbtw@mail.ru>
2025-02-18 18:33:39 +0700

    ...

--- SpeedSolverFrontend/src/App.tsx
+++ SpeedSolverFrontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import WelcomePage from "./pages/welcome/WelcomePage"
 import { LoginPage } from "./pages/access/login/LoginPage"
+import { RegisterPage } from "./pages/access/register/RegisterPage"
 function App() {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
     </Routes>  
   )
 }
--- SpeedSolverFrontend/src/app/axios_api.ts
+++ SpeedSolverFrontend/src/app/axios_api.ts
@@ -18,3 +18,16 @@ export const authorize = (username: string, password: string) => {
         return Promise.reject("Ошибка на стороне клиента.")
     })
 }
+
+export const register = (username: string, password: string) => {
+    return client.post("/access/register", {
+        username: username,
+        password: password
+    }).then(response => response.data)
+    .catch(error => {
+        if (error.response) {
+            return Promise.reject(error.response.data.detail) || "Неизвестная ошибка"
+        }
+        return Promise.reject("Ошибка на стороне клиента.")
+    })
+}
